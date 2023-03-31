255442010d5b9f5d8cca1944e4255863fea22c0c
Kavish Boyal <38506513+universekavish@users.noreply.github.com>
2023-03-31 23:28:41 +0530

    addeditNote method put request

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,5 +1,5 @@
  import React, { useState, useEffect } from 'react';
- import { useParams } from 'react-router-dom';
+ import { useParams, useNavigate } from 'react-router-dom';
 //  import notes from '../assets/data';
  import { Link } from 'react-router-dom';
 //  bringing below icon as react componet
@@ -8,6 +8,8 @@
  const NotePage = (props) => {
     let params = useParams()
     let noteId = params.id
+
+    let navigate = useNavigate();
     // console.log("noteId: ", noteId)
     // let note = notes.find(note => note.id === Number(noteId))
     let [note, setNote ] = useState(null)
@@ -22,16 +24,31 @@
         setNote(data)
     }
 
+    let updateNote = async () => {
+        await fetch(`http://localhost:8000/notes/${noteId}`, {
+            method:"PUT",
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({...note, 'updated': new Date()})
+        })
+    }
+
+    let handleSubmit = () => {
+        updateNote()
+        navigate('/')
+    }
+
     return(
         <div className='note'>
             <div className='note-header'>
                 <h3>
                     <Link to={'/'}>
-                        <ArrowLeft />
+                        <ArrowLeft onClick={handleSubmit}/>
                     </Link>
                 </h3>
             </div>
-            <textarea value={note?.body}>
+            <textarea onChange={(e) => {setNote({...note, 'body':e.target.value})}} value={note?.body}>
 
             </textarea>
         </div>
