10ad78e381e2ed8b9b658791831e4c133445b078
Kavish Boyal <38506513+universekavish@users.noreply.github.com>
2023-03-31 23:04:58 +0530

    making api calls

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,6 +1,6 @@
- import React from 'react';
+ import React, { useState, useEffect } from 'react';
  import { useParams } from 'react-router-dom';
- import notes from '../assets/data';
+//  import notes from '../assets/data';
  import { Link } from 'react-router-dom';
 //  bringing below icon as react componet
  import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg';
@@ -9,7 +9,19 @@
     let params = useParams()
     let noteId = params.id
     // console.log("noteId: ", noteId)
-    let note = notes.find(note => note.id === Number(noteId))
+    // let note = notes.find(note => note.id === Number(noteId))
+    let [note, setNote ] = useState(null)
+
+    useEffect(() => {
+        getNote()
+    }, [noteId])
+
+    let getNote = async () => {
+        let response = await fetch(`http://localhost:8000/notes/${noteId}`)
+        let data = await response.json()
+        setNote(data)
+    }
+
     return(
         <div className='note'>
             <div className='note-header'>
--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -1,8 +1,20 @@
-import React from "react";
-import notes from '../assets/data';
+import React, { useState, useEffect } from "react";
+// import notes from '../assets/data';
 import ListItem from "../components/ListItem";
 
 const NotesListPage = () => {
+    let [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        getNotes()
+    }, [])
+
+    let getNotes = async () => {
+        let response = await fetch('http://localhost:8000/notes/')
+        let data = await response.json()
+        setNotes(data)
+    }
+
     return (
         <div className="notes">
             <div className="notes-header">
